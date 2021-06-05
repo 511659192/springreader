0f7a837c8953f09eb62a5fd432ccb5be9d6d0371
Administrator <511659192>
2021-06-05 21:59:02 +0800

    1111

---
+++ spring-sources/src/main/java/com/example/springsources/SpringSourcesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSourcesApplication.class, args);
+    }
+
+}
